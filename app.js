7fdaf93ef939c40700a482cb24bd7d400d62ef32
ItachiHyuga <47303156+ItachiHyuga@users.noreply.github.com>
2019-10-07 19:47:53 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+function resizeText(multiplier) {
+    if (document.body.style.fontSize == "") {
+      document.body.style.fontSize = "1.0em";
+    }
+    document.body.style.fontSize = parseFloat(document.body.style.fontSize) + (multiplier * 0.1) + "em";
+    
+
+
+ console.log("font size")
+
+}
+function applyt(x) {
+
+document.getElementById("styles").href=x.id+".css";
+
+}
+var themes={
+    dark: {
+        pagebackground: "#000",
+        contentbackground: "#1b1b1b",
+        fontcolor: "#efeff4"
+    }
+}
+
+
+ 
