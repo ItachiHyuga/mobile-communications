5022fddbe4b4a9a802e544c500442bbb0a29d3ab
ItachiHyuga <47303156+ItachiHyuga@users.noreply.github.com>
2019-10-08 17:17:38 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,30 +1,72 @@
+
+
+document.body.style.fontSize=localStorage.getItem('fontsize')
+
+document.body.style.textAlign=localStorage.getItem('justification')
+
+document.getElementById("styles").href=localStorage.getItem('theme')+".css"
+//fontsize
 function resizeText(multiplier) {
-    if (document.body.style.fontSize == "") {
-      document.body.style.fontSize = "1.0em";
-    }
-    document.body.style.fontSize = parseFloat(document.body.style.fontSize) + (multiplier * 0.1) + "em";
-    
+  if (document.body.style.fontSize == "") {
+    document.body.style.fontSize = "1.0em";
+  }
+  fz=parseFloat(document.body.style.fontSize) + (multiplier * 0.1) + "em"
+  document.body.style.fontSize = fz;
+  localStorage.setItem("fontsize",fz);
 
 
- console.log("font size")
+
+  console.log("font size "+localStorage.getItem('fontsize'));
 
 }
+
+
+//theme
 function applyt(x) {
 
-document.getElementById("styles").href=x.id+".css";
+  document.getElementById("styles").href = x.id + ".css";
+  localStorage.setItem("theme",x.id);
 
 }
 
+//justify
 function justify() {
 
-    document.body.style.textAlign="justify";
-    
-    }
+  document.body.style.textAlign = "justify";
+  localStorage.setItem("justification","justify");
 
-    function unjustify() {
+}
 
-        document.body.style.textAlign="left";
-        
-        }
+function unjustify() {
 
-        
+  document.body.style.textAlign = "left";
+  localStorage.setItem("justification","left");
+
+}
+
+function restoredefaults() {
+
+  
+document.body.style.fontSize="1.0em";
+
+localStorage.setItem('fontsize',"1.0em");
+
+document.body.style.textAlign="left";
+
+localStorage.setItem('justification','left');
+
+document.getElementById("styles").href="null.css"
+
+localStorage.setItem('theme',"none");
+
+
+}
+
+
+if (typeof (Storage) != "undefined") {
+
+} else {
+  alert("Settings can't be saved. Please update the app.")
+}
+
+  
